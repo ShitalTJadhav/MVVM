00a87ec259adc4d54438c4be1d4ec45228d469b0
Shital Jadhav <shital.jadhavt11@gmail.com>
2019-01-09 15:23:30 +0100

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Tushar  Jadhav on 2019-01-09.
+//  Copyright © 2019 Shital  Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
